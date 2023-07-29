f3d735fc52e0ddef940a5f9225686b2a4d57f91c
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2023-07-29 22:41:34 +0900

    [add] ユーザー登録のフォームリクエストを作成

---
+++ src/app/Http/Requests/UserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'     => ['required', 'string', 'max:50'],
+            'email'    => ['required', 'string', 'email', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8', 'alpha_num:ascii', 'confirmed'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $res = response()->json([
+            'errors' => $validator->errors(),
+            ],
+            400);
+        throw new HttpResponseException($res);
+    }
+}
