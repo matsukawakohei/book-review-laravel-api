60dd2df6c7199c56eacc79e28da88096d705fd5c
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2023-07-30 22:29:08 +0900

    [add] レビュー投稿のフォームリクエストを作成

---
+++ src/app/Http/Requests/ReviewRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class ReviewRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'isbn'    => ['required', 'string', 'max:20', 'regex:/^[0-9-]+$/'],
+            'comment' => ['required', 'string'],
+            'point'   => ['required', 'between:0,5', 'decimal:1']
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $res = response()->json([
+            'errors' => $validator->errors(),
+            ],
+            400);
+        throw new HttpResponseException($res);
+    }
+}
