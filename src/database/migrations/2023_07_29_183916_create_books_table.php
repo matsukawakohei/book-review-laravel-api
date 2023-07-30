49dd6383ebb44b55ed8afe15f7619e3c127fe3e2
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2023-07-30 22:28:27 +0900

    [fix] booksテーブルにタイムスタンプを追加

--- src/database/migrations/2023_07_29_183916_create_books_table.php
+++ src/database/migrations/2023_07_29_183916_create_books_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('isbn', 20)->nullable();
+            $table->timestamps();
         });
     }
 
