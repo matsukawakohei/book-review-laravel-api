51daadc86d94fe32ffe37cecd0b1930c50de430c
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2023-08-13 16:42:30 +0900

    [add] ほしいものリスト登録のフォームリクエストを作成

---
+++ src/app/Http/Requests/WishListRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class WishListRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'isbn' => ['required', 'string', 'max:20', 'regex:/^[0-9-]+$/'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $res = response()->json([
+            'errors' => $validator->errors(),
+            ],
+            400);
+        throw new HttpResponseException($res);
+    }
+}
