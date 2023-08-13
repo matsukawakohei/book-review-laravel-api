6378006ae910182c949a16e6fb03d3cf65c656e5
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2023-08-13 16:41:25 +0900

    [fix] ほしいものリストのリレーションと許可するパラメータを追加

--- src/app/Models/WishList.php
+++ src/app/Models/WishList.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WishList extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'book_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book(): BelongsTo
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
