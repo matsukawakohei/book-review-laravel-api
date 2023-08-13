6e558d535a34a9b70da34078365397f1c00a371a
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2023-08-13 16:11:51 +0900

    [fix] ファイル名とクラス名の誤記修正

--- src/database/factories/WishListFactory.php
+++ src/database/factories/WishListFactory.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\WithList>
  */
-class WithListFactory extends Factory
+class WishListFactory extends Factory
 {
     /**
      * Define the model's default state.
